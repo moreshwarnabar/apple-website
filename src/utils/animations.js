f4f9ba81bdd2a037f86646eaf6bbe734da3afcdb
moreshwarnabar <mrnabar@gmail.com>
2024-03-31 10:56:07 -0700

    created the features section

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,3 +1,21 @@
+import gsap from "gsap";
+
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export const animateWithGsap = (target, animProps, scrollProps) => {
+  gsap.to(target, {
+    ...animProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: "restart reverse restart reverse",
+      start: "top 85%",
+      ...scrollProps,
+    },
+  });
+};
+
 export const animateWithGsapTimeline = (
   timeline,
   rotRef,

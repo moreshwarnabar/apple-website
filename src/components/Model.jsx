005f9881a2b7538506ef9c56e367666f8994a501
moreshwarnabar <mrnabar@gmail.com>
2024-03-31 08:48:29 -0700

    created the model view component

---
+++ src/components/Model.jsx
@@ -0,0 +1,119 @@
+import React, { useRef, useState } from "react";
+import * as THREE from "three";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+import ModelView from "./ModelView";
+import { yellowImg } from "../utils";
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+import { models, sizes } from "../constants";
+
+const Model = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#9f9a91", "#ffe7b9", "#6f6c64"],
+    img: yellowImg,
+  });
+
+  // camera control for the model view
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  // models
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+
+  // rotation values for each model
+  const [smallRot, setSmallRot] = useState(0);
+  const [largelRot, setLargeRot] = useState(0);
+
+  useGSAP(() => {
+    gsap.to("#heading", {
+      y: 0,
+      opacity: 1,
+    });
+  }, []);
+
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a closer look.
+        </h1>
+
+        <div className="mt-5 flex flex-col items-center">
+          <div className="relative h-[75vh] w-full overflow-hidden md:h-[90vh]">
+            <ModelView
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
+              setRotState={setSmallRot}
+              item={model}
+              size={size}
+            />
+            <ModelView
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
+              setRotState={setLargeRot}
+              item={model}
+              size={size}
+            />
+
+            <Canvas
+              className="size-full"
+              style={{
+                position: "fixed",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                overflow: "hidden",
+              }}
+              eventSource={document.getElementById("root")}
+            >
+              <View.Port />
+            </Canvas>
+          </div>
+
+          <div className="mx-auto w-full">
+            <p className="mb-5 text-center text-sm font-light">{model.title}</p>
+            <div className="flex-center">
+              <ul className="color-container">
+                {models.map((m, i) => (
+                  <li
+                    key={i}
+                    className="mx-2 size-6 cursor-pointer rounded-full"
+                    style={{ backgroundColor: m.color[0] }}
+                    onClick={() => setModel(m)}
+                  />
+                ))}
+              </ul>
+              <button className="size-btn-container">
+                {sizes.map(({ label, value }) => (
+                  <span
+                    key={label}
+                    className="size-btn"
+                    style={{
+                      backgroundColor: size === value ? "white" : "transparent",
+                      color: size === value ? "black" : "white",
+                    }}
+                    onClick={() => setSize(value)}
+                  >
+                    {label}
+                  </span>
+                ))}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Model;
---
+++ src/components/ModelView.jsx
@@ -0,0 +1,56 @@
+import * as THREE from "three";
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
+import React, { Suspense } from "react";
+import Lights from "./Lights";
+import IPhone from "./IPhone";
+import Loader from "./Loader";
+
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotState,
+  size,
+  item,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`size-full border-2 border-red-500 ${index === 2 ? "-right-full" : ""}`}
+    >
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotState(controlRef.current.getAzimuthalAngle())}
+      />
+
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large'`}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<Loader />}>
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
+    </View>
+  );
+};
+
+export default ModelView;
